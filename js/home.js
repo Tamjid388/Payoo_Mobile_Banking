267dbb30b14bc72d5c558d43ea13c3bfbf3ecee5
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2024-09-20 22:01:01 +0600

    Initial commit

---
+++ js/home.js
@@ -0,0 +1,10 @@
+document.getElementById('btn-add-money')
+.addEventListener('click',function(e){
+    e.preventDefault();
+    const addMoneyInput=document.getElementById('input-add-money').value
+    console.log(addMoneyInput);
+
+    // Get the pin number
+    const pinNumberInput=document.getElementById('input-pin-number').value
+
+})
---
+++ js/login.js
@@ -0,0 +1,20 @@
+document.getElementById('btn-login').addEventListener('click',
+    function(event){
+       event.preventDefault();
+
+
+    //    get the number
+    const phonenumber=document.getElementById('phone-number').value
+   
+
+   const pinNumber=document.getElementById('pin-number').value;
+   console.log(phonenumber,pinNumber);
+   if(phonenumber ==='1234' && pinNumber==='1234'){
+    console.log("You are logged in");
+    window.location.href='home.html'
+   }
+   else{
+    alert("Incorrect pin or number")
+   }
+}
+)
