7fb796919adbd4a46ea88cedcff48acee5ddea2a
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2024-09-20 22:57:35 +0600

    add money complete

--- js/home.js
+++ js/home.js
@@ -5,6 +5,30 @@ document.getElementById('btn-add-money')
     console.log(addMoneyInput);
 
     // Get the pin number
-    const pinNumberInput=document.getElementById('input-pin-number').value
+    const pinNumberInput=document.getElementById('input-pin-number').
+    value
+    console.log(pinNumberInput);
+
+    // Varify Pin
+    if(pinNumberInput==='1234'){
+        console.log("Adding Money");
+        // Getting the existing balance
+      const balance=
+        document.getElementById('account-balance').innerText;
+        console.log(balance);
+
+
+        // New Balance
+        const addMoneyNumber=parseFloat(addMoneyInput);
+        const balanceNumber=parseFloat(balance)
+        const newBalance=(addMoneyNumber+balanceNumber)
+        console.log(newBalance);
+
+        // Update balance in UI
+        document.getElementById('account-balance').innerText=newBalance
+
+    }else{
+        alert("Please Enter Valid Pin")
+    }
 
 })
